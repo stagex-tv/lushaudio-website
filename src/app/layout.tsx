90c6f34da0678736df6431f1d32f5ee23a2f597e
Julian Zdzienicki <julian@stagex.tv>
2026-04-05 01:20:59 -0400

    Revert "Add subtle background scene with mountains, clouds and waves; fix hero text"
    This reverts commit 12a7dfe24ac46aecdbc17a5d33eeb7b3d82217e5.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import BackgroundScene from "./components/BackgroundScene";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +28,6 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-[#0a0a0a] text-white">
-        <BackgroundScene />
         {children}
       </body>
     </html>
